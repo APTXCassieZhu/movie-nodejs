0e88db12140e6b48622be0fc09e2533e860bbdf5
ruiqi <ruiqi.zhu@stonybrook.edu>
2021-03-26 21:48:43 -0700

    set up angular

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MylistComponent } from './mylist/mylist.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { MoviepageComponent } from './components/moviepage/moviepage.component';
+import { TvpageComponent } from './components/tvpage/tvpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MylistComponent,
+    HomepageComponent,
+    MoviepageComponent,
+    TvpageComponent
   ],
   imports: [
     BrowserModule,
