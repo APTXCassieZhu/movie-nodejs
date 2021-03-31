52ac3a9150f6d523981e4b1ab68cad246ba418a5
ruiqi <ruiqi.zhu@stonybrook.edu>
2021-03-31 02:27:16 -0700

    outline: none; media details

--- front-end/src/app/components/child-id/child-id.component.ts
+++ front-end/src/app/components/child-id/child-id.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 import { DetailsService } from "../../services/details.service"
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
+
 
 let apiLoaded = false;
 export interface media{
@@ -15,6 +20,19 @@ export interface media{
   "spoken_languages": string[]
 }
 
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [{
+  type: 'success',
+  message: 'Added to watchlist.',
+}, {
+  type: 'danger',
+  message: 'Removed from watchlist.',
+}];
+
 @Component({
   selector: 'app-child-id',
   templateUrl: './child-id.component.html',
@@ -25,7 +43,13 @@ export class ChildIdComponent implements OnInit {
   public media_type: any;
   public cur_media: media = {} as media;
   public duration: string = '';
+  public added: boolean = false;
+  public alert: Alert = {} as Alert;
+  public showAlert = '';
+  private _success = new Subject<string>();
   constructor(private route: ActivatedRoute, private detailsService: DetailsService) { }
+  
+  @ViewChild('selfClosingAlert', {static: false}) selfClosingAlert: NgbAlert = {} as NgbAlert; 
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -44,8 +68,27 @@ export class ChildIdComponent implements OnInit {
       var minutes = this.cur_media.runtime[0] % 60;
       this.duration = hours + 'hrs ' + minutes + 'mins';
     })
+
+    this._success.subscribe(message => this.showAlert = message);
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if(this.selfClosingAlert){
+        this.selfClosingAlert.close();
+      }
+    });
   }
-  addToWatchList(){
-    
+
+  addToWatchList(event: any){
+    this.showAlert = '';
+    this.added = true;
+    this.alert = ALERTS[0];
+    this._success.next(`${new Date()} - Message successfully changed.`); 
+    console.log("added");
+  }
+  removeFromWatchList(event: any){
+    this.showAlert = '';
+    this.added = false;
+    this.alert = ALERTS[1];
+    this._success.next(`${new Date()} - Message successfully changed.`); 
+    console.log("removed");
   }
 }
