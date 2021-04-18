04245dbfe8faec6d9b9c92c92cca87a232b877a0
ruiqi <ruiqi.zhu@stonybrook.edu>
2021-04-17 17:28:39 -0700

    请求video和请求slide放一起，解决异步问题

--- back-end/routes/ios.js
+++ back-end/routes/ios.js
@@ -34,67 +34,21 @@ router.get('/now_playing', function(req, res){
 })
 
 //top rated movies
-router.get('/toprated/movie', function(req, res){
+router.get('/toprated/movie', async function(req, res){
     var api_key = config.API_KEY;  
     let url = "https://api.themoviedb.org/3/movie/top_rated?api_key="+api_key+"&language=en-US&page=1";
-    axios.get(url).then(data => {
-        var result = '{"results":['  
-        var len = data.data.results.length;
-        len = Math.min(len, 20);
-        for(var i = 0; i < len; i++){
-            result += '{'
-                + '"id":' + data.data.results[i].id + ','
-                + '"title":"' + data.data.results[i].title + '",'
-                + '"date":"' + data.data.results[i].release_date.slice(0, 4) + '",'
-                + '"media_type":"movie",';
-            if(data.data.results[i].poster_path){
-                result += '"poster_path":"https://image.tmdb.org/t/p/w500' + data.data.results[i].poster_path + '"';
-            }else{
-                result += '"poster_path":"https://cinemaone.net/images/movie_placeholder.png"';
-            }
-            if(i != len-1){
-                result += '},';
-            }else{
-                result += '}';
-            }
-        }        
-        result += ']}';        
-        res.json(JSON.parse(result));
-    }).catch(err => {
-        res.send(err);
-    })
+    const data = await axios.get(url);
+    var result = await getMovieList(data);    
+    res.json(JSON.parse(result));
 })
 
 //popular movies
-router.get('/popular/movie', function(req, res){
+router.get('/popular/movie', async function(req, res){
     var api_key = config.API_KEY;    
     let url = "https://api.themoviedb.org/3/movie/popular?api_key="+api_key+"&language=en-US&page=1";
-    axios.get(url).then(data => {        
-        var result = '{"results":['  
-        var len = data.data.results.length;
-        len = Math.min(len, 20);
-        for(var i = 0; i < len; i++){
-            result += '{'
-                + '"id":' + data.data.results[i].id + ','
-                + '"title":"' + data.data.results[i].title + '",'
-                + '"date":"' + data.data.results[i].release_date.slice(0, 4) + '",'
-                + '"media_type":"movie",';
-            if(data.data.results[i].poster_path){
-                result += '"poster_path":"https://image.tmdb.org/t/p/w500' + data.data.results[i].poster_path + '"';
-            }else{
-                result += '"poster_path":"https://cinemaone.net/images/movie_placeholder.png"';
-            }
-            if(i != len-1){
-                result += '},';
-            }else{
-                result += '}';
-            }
-        }        
-        result += ']}';        
-        res.json(JSON.parse(result));
-    }).catch(err => {
-        res.send(err);
-    })
+    const data = await axios.get(url);
+    var result = await getMovieList(data);
+    res.json(JSON.parse(result));
 })
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -114,7 +68,6 @@ router.get('/trending/tv', function(req, res){
             result += '{'
                 + '"id":' + data.data.results[i].id + ','
                 + '"title":"' + data.data.results[i].name + '",'
-                + '"date":"' + data.data.results[i].first_air_date.slice(0, 4) + '",'
                 + '"media_type":"tv",';
             if(data.data.results[i].poster_path){
                 result += '"poster_path":"https://image.tmdb.org/t/p/w500' + data.data.results[i].poster_path + '"';
@@ -135,65 +88,118 @@ router.get('/trending/tv', function(req, res){
 })
 
 //popular tv
-router.get('/popular/tv', function(req, res){
+router.get('/popular/tv', async function(req, res){
     var api_key = config.API_KEY;    
     let url = "https://api.themoviedb.org/3/tv/popular?api_key="+api_key+"&language=en-US&page=1";
-    axios.get(url).then(data => {
-        var result = '{"results":['  
-        var len = data.data.results.length;
-        len = Math.min(len, 20);
-        for(var i = 0; i < len; i++){
-            result += '{'
-                + '"id":' + data.data.results[i].id + ','
-                + '"title":"' + data.data.results[i].name + '",'
-                + '"date":"' + data.data.results[i].first_air_date.slice(0, 4) + '",'
-                + '"media_type":"tv",';
-            if(data.data.results[i].poster_path){
-                result += '"poster_path":"https://image.tmdb.org/t/p/w500' + data.data.results[i].poster_path + '"';
-            }else{
-                result += '"poster_path":"https://cinemaone.net/images/movie_placeholder.png"';
-            }
-            if(i != len-1){
-                result += '},';
-            }else{
-                result += '}';
-            }
-        }        
-        result += ']}';
-        res.json(JSON.parse(result));
-    }).catch(err => {
-        res.send(err);
-    })
+    const data = await axios.get(url)
+    var result = await getTVList(data);
+    res.json(JSON.parse(result));
 })
 
 //top rated tv
-router.get('/toprated/tv', function(req, res){
+router.get('/toprated/tv', async function(req, res){
     var api_key = config.API_KEY;    
     let url = "https://api.themoviedb.org/3/tv/top_rated?api_key="+api_key+"&language=en-US&page=1";
-    axios.get(url).then(data => {
-        var result = '{"results":['  
-        var len = data.data.results.length;
-        len = Math.min(len, 20);
-        for(var i = 0; i < len; i++){
-            result += '{'
-                + '"id":' + data.data.results[i].id + ','
-                + '"title":"' + data.data.results[i].name + '",'
-                + '"media_type":"tv",';
-            if(data.data.results[i].poster_path){
-                result += '"poster_path":"https://image.tmdb.org/t/p/w500' + data.data.results[i].poster_path + '"';
-            }else{
-                result += '"poster_path":"https://cinemaone.net/images/movie_placeholder.png"';
-            }
-            if(i != len-1){
-                result += '},';
-            }else{
-                result += '}';
-            }
-        }        
-        result += ']}';
-        res.json(JSON.parse(result));
-    }).catch(err => {
-        res.send(err);
-    })
+    const data = await axios.get(url)
+    var result = await getTVList(data);
+    res.json(JSON.parse(result));
 })
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/// helper function
+async function getMovieList(data){
+    var result = '{"results":['  
+    var len = data.data.results.length;
+    len = Math.min(len, 20);
+    for(var i = 0; i < len; i++){     
+        result += '{'
+            + '"id":' + data.data.results[i].id + ','
+            + '"title":"' + data.data.results[i].title + '",'
+            + '"date":"' + data.data.results[i].release_date.slice(0, 4) + '",'
+            + '"media_type":"movie",'
+        result += '"video":'+ await getVideoByID(data.data.results[i].id, 'movie')+','
+        if(data.data.results[i].poster_path){
+            result += '"poster_path":"https://image.tmdb.org/t/p/w500' + data.data.results[i].poster_path + '"';
+        }else{
+            result += '"poster_path":"https://cinemaone.net/images/movie_placeholder.png"';
+        }
+        if(i != len-1){
+            result += '},';
+        }else{
+            result += '}';
+        }
+    }        
+    result += ']}';      
+    return result;
+}
+
+async function getTVList(data){
+    var result = '{"results":['  
+    var len = data.data.results.length;
+    len = Math.min(len, 20);
+    for(var i = 0; i < len; i++){     
+        result += '{'
+            + '"id":' + data.data.results[i].id + ','
+            + '"title":"' + data.data.results[i].name + '",'
+            + '"date":"' + data.data.results[i].first_air_date.slice(0, 4) + '",'
+            + '"media_type":"tv",'
+        result += '"video":'+ await getVideoByID(data.data.results[i].id, 'tv')+','
+        if(data.data.results[i].poster_path){
+            result += '"poster_path":"https://image.tmdb.org/t/p/w500' + data.data.results[i].poster_path + '"';
+        }else{
+            result += '"poster_path":"https://cinemaone.net/images/movie_placeholder.png"';
+        }
+        if(i != len-1){
+            result += '},';
+        }else{
+            result += '}';
+        }
+    }        
+    result += ']}';      
+    return result;
+}
+
+async function getVideoByID(id, type){    
+    var api_key = config.API_KEY; 
+    let url = "https://api.themoviedb.org/3/"+type+"/"+id+"/videos?api_key="+api_key+"&language=en-US&page=1";
+    var result = '';
+    const data = await axios.get(url);
+    result = '{'
+    var len = data.data.results.length;
+    var findTrailer = 0;
+    for(var i = 0; i < len; i++){
+        if(data.data.results[i].type == 'Trailer'){
+            findTrailer = 1;
+            result += '"site": "' + data.data.results[i].site + '",'
+                + '"type": "' + data.data.results[i].type + '",'
+                + '"name": "' + data.data.results[i].name + '",'
+                + '"key": "' + data.data.results[i].key + '"}';
+            break;
+        }
+    }  
+    if(findTrailer == 0){
+        for(var i = 0; i < len; i++){
+            if(data.data.results[i].type == 'Teaser'){
+                result += '"site": "' + data.data.results[i].site + '",'
+                    + '"type": "' + data.data.results[i].type + '",'
+                    + '"name": "' + data.data.results[i].name + '",'
+                    + '"key": "' + data.data.results[i].key + '"}';
+                break;
+            }
+        }  
+    }
+    if(result == '{'){
+        result += '"site": "Youtube",'
+            + '"type": "fake",'
+            + '"name": "undefined",' 
+            + '"key": "tzkWB85ULJY"}';
+    }    
+    return result
+}
+
 module.exports = router;
